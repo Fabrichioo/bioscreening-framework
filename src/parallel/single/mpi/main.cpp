d654f61407fe0127e26b8e8ed8e480a54c8be0fe
Fabricio <fabricioduarte1420@gmail.com>
2025-04-14 17:42:12 +0200

    Versión MPI funcional sin distribución de datos

--- src/parallel/single/mpi/main.cpp
+++ src/parallel/single/mpi/main.cpp
@@ -8,6 +8,55 @@
 #include "Utils.h"
 #include <mpi.h>
 
+void mpi_docking(const std::vector<Molecule>& proteins,
+                 const std::vector<Molecule>& ligands,
+                 std::vector<float>& scores) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        std::cout << "Ejecutando docking con MPI..." << std::endl;
+        std::cout << "Número total de procesos: " << size << std::endl;
+    }
+
+    size_t total = proteins.size() * ligands.size();
+    size_t chunk = total / size;
+    size_t remainder = total % size;
+
+    // Calcular el rango de índices a procesar por cada proceso
+    size_t start = rank * chunk + (rank < remainder ? rank : remainder);
+    size_t end = start + chunk + (rank < remainder ? 1 : 0);
+
+    std::vector<float> localScores(end - start);
+    for (size_t idx = start; idx < end; ++idx) {
+        size_t i = idx / ligands.size();
+        size_t j = idx % ligands.size();
+        localScores[idx - start] = performDocking(proteins[i], ligands[j]);
+    }
+
+    // El proceso 0 reserva el espacio para almacenar todos los resultados
+    if (rank == 0) {
+        scores.resize(total);
+    }
+
+    // Preparar arreglos para el envío de datos
+    std::vector<int> recvCounts(size);
+    std::vector<int> displs(size);
+    int localSize = static_cast<int>(localScores.size());
+    MPI_Gather(&localSize, 1, MPI_INT, recvCounts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        displs[0] = 0;
+        for (int i = 1; i < size; ++i) {
+            displs[i] = displs[i - 1] + recvCounts[i - 1];
+        }
+    }
+
+    MPI_Gatherv(localScores.data(), localSize, MPI_FLOAT,
+                scores.data(), recvCounts.data(), displs.data(), MPI_FLOAT, 0, MPI_COMM_WORLD);
+}
+
 int main(int argc, char* argv[]) {
 
     MPI_Init(&argc, &argv);
@@ -31,60 +80,23 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    Timer timer;
-    timer.start();
-
+    double t1, t2;
+    MPI_Barrier(MPI_COMM_WORLD);
+    t1 = MPI_Wtime();
+    
     std::vector<float> scores;
+    mpi_docking(proteins, ligands, scores);
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    t2 = MPI_Wtime();
 
-    int rank, size;
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
     if (rank == 0)
-        std::cout << "Ejecutando docking con MPI..." << std::endl;
-
-    size_t total = proteins.size() * ligands.size();
-    // Distribución del trabajo entre procesos
-    size_t chunk = total / size;
-    size_t remainder = total % size;
-
-    size_t start = rank * chunk + (rank < remainder ? rank : remainder);
-    size_t end = start + chunk + (rank < remainder ? 1 : 0);
-
-    std::vector<float> localScores(end - start);
-
-    for (size_t idx = start; idx < end; ++idx) {
-        size_t i = idx / ligands.size();
-        size_t j = idx % ligands.size();
-        localScores[idx - start] = performDocking(proteins[i], ligands[j]);
-    }
-
-    if(rank == 0) {
-        scores.resize(total);
-    }
-
-    // Preparar recuento y desplazamientos para la recolección de datos
-    std::vector<int> recvCounts(size);
-    std::vector<int> displs(size);
-    int localSize = localScores.size();
-    MPI_Gather(&localSize, 1, MPI_INT, recvCounts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    if(rank == 0) {
-        displs[0] = 0;
-        for (int i = 1; i < size; ++i) {
-            displs[i] = displs[i - 1] + recvCounts[i - 1];
-        }
-    }
-
-    MPI_Gatherv(localScores.data(), localSize, MPI_FLOAT,
-                scores.data(), recvCounts.data(), displs.data(), MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    timer.stop();
-    std::cout << "Tiempo de ejecución: " << timer.elapsedMilliseconds() << " ms" << std::endl;
-
-    if(verbose)
+        std::cout << "Tiempo de ejecución: " << t2 - t1 << " s" << std::endl;
+    if (rank == 0 && verbose)
         analyzeDockingResults(scores, proteins.size(), ligands.size());
     
     MPI_Finalize();
-	return(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
